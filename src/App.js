33cfbf7d70582a1c5c0455584375fb479cf17304
Ardrasajan <ardrasajan7@gmail.com>
2024-09-11 11:46:43 +0530

    home page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Homepage from './Components/Homepage';
 import {BrowserRouter,Route,Routes} from 'react-router-dom'
-
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route/>
+        <Route path="/Homepage" element={<Homepage/>}/>
       </Routes>
     </BrowserRouter>
   

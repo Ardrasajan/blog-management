9a10591f441e3b7ad128d179996a71a0ea5b65b7
Rajiramakrishnan <rajirajeswari50@gmail.com>
2024-09-11 11:12:27 +0530

    admin login.js added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import AdminLogin from './Components/Admin/AdminLogin';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route/>
+        <Route path='/adminlogin' Component={<AdminLogin/>}/>
       </Routes>
     </BrowserRouter>
   );
--- src/Components/Admin/AdminLogin.js
+++ src/Components/Admin/AdminLogin.js
@@ -1,8 +1,39 @@
 import React from 'react'
+import "../../Assets/Style/adminlogin.css"
+import { FaUser } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
+
 
 function AdminLogin() {
   return (
-    <div>AdminLogin</div>
+    <div className='Admin_login_component'>
+      <div className='Admin_login_container'>
+        <div className='Admin_login_form'>
+          <form>
+            <h1>Admin Login</h1>
+            <div className='Admin_login_input'>
+              <input type='text' placeholder='username' required/>
+              <FaUser className='Admin_login_icon'/>
+              </div>
+              <div className='Admin_login_input'>
+                <input type='password' placeholder='password' required/>
+                <FaLock className='Admin_login_icon'/>
+                </div>
+                <div className='Admin_login_remember'>
+                  <label>
+                    <input type="checkbox"/>Remember me
+                  </label>
+                  <a href="#">Forgot password?</a>
+                  </div>
+                  <button type="submit">login</button>
+                  
+          </form>
+
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
